0d07a4fc28b7fc35898c0e202b2326b3388c5793
Aurore Gaucher <gaucher.aurore@laposte.net>
2017-03-16 12:10:31 +0100

    add entities

--- src/Domain/Author.php
+++ src/Domain/Author.php
@@ -5,5 +5,75 @@ namespace MyBooksApp\Domain;
 
 class Author
 {
+    /**
+     * Author id
+     * @var integer
+     */
+    private $id;
 
+    /**
+     * Author first name
+     * @var string
+     */
+    private $firstName;
+
+    /**
+     * Author last name
+     * @var string
+     */
+    private $lastName;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     * @return $this
+     */
+    public function setFirstName(string $firstName)
+    {
+        $this->firstName = $firstName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     * @return $this
+     */
+    public function setLastName(string $lastName)
+    {
+        $this->lastName = $lastName;
+        return $this;
+    }
 }
--- src/Domain/Book.php
+++ src/Domain/Book.php
@@ -5,5 +5,123 @@ namespace MyBooksApp\Domain;
 
 class Book
 {
+    /**
+     * Book id
+     * @var integer
+     */
+    private $id;
 
+    /**
+     * Book title
+     * @var string
+     */
+    private $title;
+
+    /**
+     * Book ISBN
+     * @var string
+     */
+    private $isbn;
+
+    /**
+     * Book summary
+     * @var string
+     */
+    private $summary;
+
+    /**
+     * Author of the book
+     * @var Author
+     */
+    private $author;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return $this
+     */
+    public function setId(int $id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIsbn()
+    {
+        return $this->isbn;
+    }
+
+    /**
+     * @param string $isbn
+     * @return $this
+     */
+    public function setIsbn(string $isbn)
+    {
+        $this->isbn = $isbn;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSummary()
+    {
+        return $this->summary;
+    }
+
+    /**
+     * @param string $summary
+     * @return $this
+     */
+    public function setSummary(string $summary)
+    {
+        $this->summary = $summary;
+        return $this;
+    }
+
+    /**
+     * @return Author
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param Author $author
+     * @return $this
+     */
+    public function setAuthor(Author $author)
+    {
+        $this->author = $author;
+        return $this;
+    }
 }
