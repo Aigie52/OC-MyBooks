970f714e031a606e497444bb98543819cbc36068
Aurore Gaucher <gaucher.aurore@laposte.net>
2017-03-16 11:53:43 +0100

    init app

--- app/app.php
+++ app/app.php
@@ -1,2 +1,20 @@
 <?php
 
+// Register global errors and exception handlers
+use Silex\Provider\AssetServiceProvider;
+use Silex\Provider\DoctrineServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Symfony\Component\Debug\ErrorHandler;
+use Symfony\Component\Debug\ExceptionHandler;
+
+ErrorHandler::register();
+ExceptionHandler::register();
+
+// Register service providers
+$app->register(new DoctrineServiceProvider());
+$app->register(new TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+));
+$app->register(new AssetServiceProvider(), array(
+    'assets.version' => 'v1'
+));
--- app/config/dev.php
+++ app/config/dev.php
@@ -1,2 +1,7 @@
 <?php
 
+// Include the prod config
+require __DIR__.'/prod.php';
+
+// Enable the debug mode
+$app['debug'] = true;
